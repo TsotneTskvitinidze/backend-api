7399325774e3c7cde0e3f0f8714510252d0bff21
Tsotne Tskvitinidze <tskvitinidzetsotne@gmail.com>
2023-10-18 15:36:37 +0400

    created database structure

---
+++ database/migrations/2023_10_18_102101_create_teacher_group_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teacher_group', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('group_id');
+            $table->unsignedBigInteger('teacher_id');
+            $table->foreign('group_id')->references('id')->on('groups');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+            $table->timestamps();
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teacher_group');
+    }
+};
---
+++ database/migrations/2023_10_18_105624_create_group_student_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group_student', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('group_id');
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('group_id')->references('id')->on('groups');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_student');
+    }
+};
